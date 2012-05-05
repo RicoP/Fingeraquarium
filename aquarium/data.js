904e96d96a0183a0da85dfe055b20f588ea2acab
thecom <oluensdorf@gmail.com>
2012-05-05 19:03:00 +0200

    Enhanced resource loading.

---
+++ aquarium/data.js
@@ -0,0 +1,102 @@
+data = {
+    'fish1': {
+        'type': 'fish',
+        'texture': 'images/fish1.png',
+        'max_age': [3 * 60, 10 * 60],
+        'energy': [50, 100],
+        'avg_speed': [10, 20],
+        'breed_time': [10, 30],
+    },
+
+    'pinkfish': {
+        'type': 'fish',
+        'texture': 'images/pinkfish.png',
+        'max_age': [3 * 60, 10 * 60],
+        'energy': [100, 100],
+        'avg_speed': [10, 10],
+        'breed_time': [10, 30],
+    },
+/*
+    'fish2': {
+        'type': 'fish',
+        'texture': 'images/fish2.png',
+        'min_age':       3 * 60,
+        'max_age':       10 * 60,
+        'min_energy':    100,
+        'max_energy':    100,
+        'min_avg_speed': 10,
+        'max_avg_speed': 30,
+        'min_breedtime': 10,
+        'max_breedtime': 30
+    },
+
+    'fish4': {
+        'type': 'fish',
+        'texture': 'images/fish4.png',
+        'min_age':       3 * 60,
+        'max_age':       10 * 60,
+        'min_energy':    100,
+        'max_energy':    100,
+        'min_avg_speed': 10,
+        'max_avg_speed': 30,
+        'min_breedtime': 10,
+        'max_breedtime': 30
+    },
+
+    'fish3': {
+        'type': 'fish',
+        'texture': 'images/fish3.png',
+        'max_age': [3 * 60, 10 * 60],
+        'energy': [100, 100],
+        'min_avg_speed': 20,
+        'max_avg_speed': 30,
+        'min_breedtime': 10,
+        'max_breedtime': 30
+    },
+
+    'seahorse': {
+        'type': 'fish',
+        'texture': 'images/seahorse.png',
+        'min_age':       3 * 60,
+        'max_age':       6 * 60,
+        'min_energy':    100,
+        'max_energy':    100,
+        'min_avg_speed': 10,
+        'max_avg_speed': 20,
+        'min_breedtime': 10,
+        'max_breedtime': 30
+    },
+
+    'jaguarshark': {
+        'type': 'fish',
+        'texture': 'images/jaguarshark.png',
+        'min_age':       3 * 60,
+        'max_age':       6 * 60,
+        'min_energy':    100,
+        'max_energy':    100,
+        'min_avg_speed': 20,
+        'max_avg_speed': 30,
+        'min_breedtime': 10,
+        'max_breedtime': 30
+    },*/
+
+    'castle': {
+        'type': 'feature',
+        'texture': 'images/castle.png',
+        'probability': 0.1,
+    },
+    'seaweed1': {
+        'type': 'feature',
+        'texture': 'images/seaweed1.png',
+        'probability': 0.1,
+    },
+    'seaweed2': {
+        'type': 'feature',
+        'texture': 'images/seaweed2.png',
+        'probability': 0.1,
+    },
+    /*this.Features = [
+        [0.1, "castle"], [0.5, "seaweed1"], [0.5, "seaweed2"], [0.5, "seaweed3"],
+        [0.1, "skull"], [0.1, "treasure"], [1, "sand1"], [1, "sand2"], [1, "sand3"]
+    ];*/
+}
